0145071e1e254361127c461ca5dfc2868af3415e
Hayotbek <idealprojectuz@gmail.com>
2023-01-04 16:00:05 +0500

    final

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+
+    freeMode: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+   
+  });
